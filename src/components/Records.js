0b4a8de728f72daed22c5edca5579ca85df6203a
Amy <yeminyi@outlook.com>
2019-06-13 12:39:52 +1000

    WIP accounting records CURD

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,33 +1,141 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Record from './Record';
+import * as RecordsAPI from '../utils/RecordsAPI';
+import RecordForm from './RecordForm';
+import AmountBox from './AmountBox';
 
-function Records(props) {
-  props = {
-    records: [
-      {"id":"1","date":"2019-06-23","title":"234","amount":1102},
-      {"id":"2","date":"2019-01-31","title":"merewrrt","amount":12},
-      {"id":"3","date":"2019-01-31","title":"11","amount":12}
-    ]
+class Records extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      records: []
+    }
   }
 
-  return (
-    <div>
-     <h2>records</h2>
-     <table border="1">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Title</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-         {props.records.map((record)=><Record record={record}/>)}
-        </tbody>
-  
-    </table>
-    </div>
-  );
+  componentDidMount() {
+    RecordsAPI.getAll().then(
+      response => this.setState({
+        records: response.data,
+        isLoaded: true
+      })
+    ).catch(
+      error => this.setState({
+        isLoaded: true,
+        error
+      })
+    )
+  }
+
+  addRecord(record) {
+    this.setState({
+      error: null,
+      isLoaded: true,
+      records: [
+        ...this.state.records,
+        record
+      ]
+    })
+  }
+
+  updateRecord(record, data) {
+    const recordIndex = this.state.records.indexOf(record);
+    const newRecords = this.state.records.map( (item, index) => {
+      if(index !== recordIndex) {
+        // This isn't the item we care about - keep it as-is
+        return item;
+      }
+
+      // Otherwise, this is the one we want - return an updated value
+      return {
+        ...item,
+        ...data
+      };
+    });
+    this.setState({
+      records: newRecords
+    });
+  }
+
+  deleteRecord(record) {
+    const recordIndex = this.state.records.indexOf(record);
+    const newRecords = this.state.records.filter( (item, index) => index !== recordIndex);
+    this.setState({
+      records: newRecords
+    });
+  }
+
+  credits() {
+    let credits = this.state.records.filter((record) => {
+      return record.amount >= 0;
+    })
+
+    return credits.reduce((prev, curr) => {
+      return prev + Number.parseInt(curr.amount, 0)
+    }, 0)
+  }
+
+  debits() {
+    let credits = this.state.records.filter((record) => {
+      return record.amount < 0;
+    })
+
+    return credits.reduce((prev, curr) => {
+      return prev + Number.parseInt(curr.amount, 0)
+    }, 0)
+  }
+
+  balance() {
+    return this.credits() + this.debits();
+  }
+
+  render() {
+    const { error, isLoaded, records } = this.state;
+    let recordsComponent;
+
+    if (error) {
+      recordsComponent = <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      recordsComponent = <div>Loading...</div>;
+    } else {
+      recordsComponent = (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record) =>
+              (<Record
+                key={record.id}
+                record={record}
+                handleEditRecord={this.updateRecord.bind(this)}
+                handleDeleteRecord={this.deleteRecord.bind(this)}
+              />)
+            )}
+          </tbody>
+        </table>
+      );
+    }
+
+    return (
+      <div className="container">
+        <h2>Records</h2>
+        <div className="row mb-3">
+          <AmountBox text="Credit" type="success" amount={this.credits()} />
+          <AmountBox text="Debit" type="danger" amount={this.debits()} />
+          <AmountBox text="Balance" type="info" amount={this.balance()} />
+        </div>
+        <RecordForm handleNewRecord={this.addRecord.bind(this)} />
+        {recordsComponent}
+      </div>
+    );
+  }
 }
 
 export default Records;
