219ca6cf6a5f22982ab593d4a941ee1bda3c7890
Amy <yeminyi@outlook.com>
2019-06-19 13:55:34 +1000

    Updates

---
+++ precache-manifest.426ab9bc1c33724f624ae896a8f1c13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a8d88a5750a249f9b71166c6853b0f",
+    "url": "/Accounting-assistant/index.html"
+  },
+  {
+    "revision": "1ffaf721f0eda30aa244",
+    "url": "/Accounting-assistant/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "02603d42139a803d7382",
+    "url": "/Accounting-assistant/static/css/main.6c0d2200.chunk.css"
+  },
+  {
+    "revision": "1ffaf721f0eda30aa244",
+    "url": "/Accounting-assistant/static/js/2.63338d5e.chunk.js"
+  },
+  {
+    "revision": "02603d42139a803d7382",
+    "url": "/Accounting-assistant/static/js/main.f0bfe7b5.chunk.js"
+  },
+  {
+    "revision": "bdefd0487aeea1850f04",
+    "url": "/Accounting-assistant/static/js/runtime~main.60584000.js"
+  }
+]);
